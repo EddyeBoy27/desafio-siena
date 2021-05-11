8dd0fb240a52cae5193ad8ac945e18f080fd84fe
eddyeboy27 <lipe0503@gmail.com>
2021-05-11 03:45:06 -0300

    Criado conexão com banco postgresql, criado arquivo de variáveis ambiente, e testado conexão com banco

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const db = require("./models/connection");
 
 const app = express();
 
@@ -8,4 +9,9 @@ app.use(express.json());
 
 const NODE_PORT = process.env.NODE_PORT || 3001;
 
-app.listen(NODE_PORT, () => console.log(`Listening on +${NODE_PORT}`));
+app.listen(NODE_PORT, async () => {
+  await db.connect();
+  const workflow = await db.query("SELECT * FROM status_tb");
+  console.log(workflow.rows);
+  console.log(`Listening on ${NODE_PORT}`);
+});
