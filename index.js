f943c2bc9f8999a6284cdcec8ff33b299375d0b9
eddyeboy27 <lipe0503@gmail.com>
2021-05-11 02:05:30 -0300

    Criado conexão node.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const NODE_PORT = process.env.NODE_PORT || 3001;
+
+app.listen(NODE_PORT, () => console.log(`Listening on +${NODE_PORT}`));
